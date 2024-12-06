320f37e428aa1e8d381438b6c5249ede3a7452bd
Devanshuverma07 <devansh00990@gmail.com>
2024-12-06 20:05:47 +0530

    patterns q's solved

---
+++ Basics/Patterns/Pattern20.java
@@ -0,0 +1,26 @@
+package Basics.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter the number of rows:");
+        int n = sc.nextInt();
+        
+        // Call the method to print the pattern
+        nLetterTriangleRev(n);
+
+        // Close the Scanner
+        sc.close();
+    }
+
+    public static void nLetterTriangleRev(int n) {
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char)('A' + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
